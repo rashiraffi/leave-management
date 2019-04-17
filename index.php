e508ab1f8218beb14263c07be97a3cc5aa929248
rashiraffi <rashi1281@gmail.com>
2019-04-17 12:17:28 +0530

    updates on login page

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
-<body >
+<body>
  <div class="container">
    <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
@@ -26,8 +26,8 @@
             <h5 class="card-title text-center">Sign In</h5>
             <form class="form-signin">
               <div class="form-label-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="inputEmail">Email address</label>
+                <input type="text" id="Userid" class="form-control" placeholder="User ID" required autofocus>
+                <label for="userid">User ID</label>
               </div>
 
               <div class="form-label-group">
