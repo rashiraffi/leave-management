44672073a7b0054a7951bf2f0e112843ce70f9d7
rashiraffi <rashi1281@gmail.com>
2019-08-29 09:27:04 +0530

    Test

--- PHP/config.php
+++ PHP/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USERNAME','rashi');
+define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_NAME','leave_management');
 $conn=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
