51a9b1a879c4994a6aaaae8e88a630438d1ad78f
rashiraffi <rashi1281@gmail.com>
2019-09-09 13:36:06 +0530

    Database Update

--- Database/leave_management.sql
+++ Database/leave_management.sql
@@ -2,10 +2,10 @@
 -- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 01, 2019 at 11:42 PM
+-- Host: localhost:3306
+-- Generation Time: Sep 09, 2019 at 01:30 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
--- PHP Version: 7.2.19-0ubuntu0.18.04.1
+-- PHP Version: 7.2.19-0ubuntu0.18.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,11 +23,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `c_id` smallint(6) NOT NULL,
+  `c_name` varchar(63) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`c_id`, `c_name`) VALUES
+(1, 'CSE_B(17-21)'),
+(2, 'CSE_A(17-21)');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `department`
 --
 
 CREATE TABLE `department` (
-  `id` int(2) NOT NULL,
+  `id` tinyint(4) NOT NULL,
   `dname` varchar(128) NOT NULL,
   `dhod` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -44,6 +63,50 @@ INSERT INTO `department` (`id`, `dname`, `dhod`) VALUES
 (5, 'Mechanical Engineering', 'Prof. Dr. Biju P. L'),
 (6, 'Mechatronics', 'update'),
 (7, 'Basic Science & Humanities', 'Prof. FR JOHN THARAYIL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `user_id` int(15) NOT NULL,
+  `stf_name` varchar(63) NOT NULL,
+  `d_id` tinyint(4) NOT NULL,
+  `c_id` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`user_id`, `stf_name`, `d_id`, `c_id`) VALUES
+(22712030, 'Namitha T N', 1, 2),
+(22712031, 'Unnikrishnan P', 1, 2),
+(22712079, 'Shaiju Paul', 1, 1),
+(22712080, 'Swapna B Sasi', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `user_id` int(15) NOT NULL,
+  `s_name` varchar(55) NOT NULL,
+  `d_id` tinyint(6) NOT NULL,
+  `c_id` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`user_id`, `s_name`, `d_id`, `c_id`) VALUES
+(11712030, 'ABC', 1, 2),
+(11712079, 'Rashi M', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -66,12 +129,8 @@ CREATE TABLE `Student_ldb` (
 --
 
 INSERT INTO `Student_ldb` (`ts`, `l_id`, `u_id`, `d_no`, `l_date`, `reason`, `status`) VALUES
-('2019-08-01 16:45:23', 8, 11712079, 2, '2019-08-02', 'Fever', 'PENDING'),
-('2019-08-01 16:50:02', 9, 100, 5, '2019-08-02', 'Internship', 'PENDING'),
-('2019-08-01 17:33:10', 10, 100, 1, '2019-09-01', 'Fever', 'PENDING'),
-('2019-08-01 17:36:25', 11, 100, 2, '2019-08-31', 'Duty Leave', 'PENDING'),
-('2019-08-01 17:36:54', 12, 100, 2, '2019-08-16', 'Fever', 'PENDING'),
-('2019-08-01 17:53:34', 13, 11712079, 2, '2019-09-05', 'Workshop', 'PENDING');
+('2019-09-09 06:41:30', 14, 11712079, 5, '2019-09-10', 'fever', 'PENDING'),
+('2019-09-09 06:41:52', 15, 11712030, 2, '2019-09-20', 'fevers', 'PENDING');
 
 -- --------------------------------------------------------
 
@@ -80,7 +139,6 @@ INSERT INTO `Student_ldb` (`ts`, `l_id`, `u_id`, `d_no`, `l_date`, `reason`, `st
 --
 
 CREATE TABLE `users` (
-  `id` int(4) NOT NULL,
   `userid` int(32) NOT NULL,
   `password` varchar(64) NOT NULL,
   `usertype` int(2) NOT NULL
@@ -90,9 +148,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `userid`, `password`, `usertype`) VALUES
-(1, 11712079, '11712079', 4),
-(2, 100, '100', 4);
+INSERT INTO `users` (`userid`, `password`, `usertype`) VALUES
+(11712030, '11712030', 4),
+(11712079, '11712079', 4),
+(22712030, '22712030', 3),
+(22712031, '22712031', 3),
+(22712079, '22712079', 3),
+(22712080, '22712080', 3);
 
 -- --------------------------------------------------------
 
@@ -120,6 +182,12 @@ INSERT INTO `usertype` (`id`, `usertype`) VALUES
 --
 
 --
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`c_id`);
+
+--
 -- Indexes for table `department`
 --
 ALTER TABLE `department`
@@ -127,17 +195,35 @@ ALTER TABLE `department`
   ADD UNIQUE KEY `dname` (`dname`);
 
 --
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `c_id` (`c_id`),
+  ADD KEY `d_id` (`d_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `student_ibfk_3` (`d_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
 -- Indexes for table `Student_ldb`
 --
 ALTER TABLE `Student_ldb`
-  ADD PRIMARY KEY (`l_id`);
+  ADD PRIMARY KEY (`l_id`),
+  ADD KEY `u_id` (`u_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `userid` (`userid`);
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `userid` (`userid`),
+  ADD KEY `usertype` (`usertype`);
 
 --
 -- Indexes for table `usertype`
@@ -151,25 +237,56 @@ ALTER TABLE `usertype`
 --
 
 --
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `c_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `Student_ldb`
 --
 ALTER TABLE `Student_ldb`
-  MODIFY `l_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `l_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `usertype`
 --
 ALTER TABLE `usertype`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`userid`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `staff_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `class` (`c_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `staff_ibfk_3` FOREIGN KEY (`d_id`) REFERENCES `department` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`userid`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_ibfk_3` FOREIGN KEY (`d_id`) REFERENCES `department` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Student_ldb`
+--
+ALTER TABLE `Student_ldb`
+  ADD CONSTRAINT `Student_ldb_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`userid`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`usertype`) REFERENCES `usertype` (`id`) ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
