48426559030a2dc7798e4349b1f1c36761dc85f3
rashiraffi <rashi1281@gmail.com>
2020-04-29 22:11:45 +0530

    DB config file update

--- PHP/config.php
+++ PHP/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USERNAME','root');
-define('DB_PASSWORD','');
+define('DB_PASSWORD','0000');
 define('DB_NAME','leave_management');
 $conn=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
 if(!$conn)
